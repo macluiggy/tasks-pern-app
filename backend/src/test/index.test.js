67d97a244489dc9b3c8f700134476d72403201c7
luiggy macias <ferrinluiggy@gmail.com>
2021-12-23 08:52:06 -0500

    adding test

---
+++ backend/src/test/index.test.js
@@ -0,0 +1,9 @@
+var chai = require("chai");
+const assert = chai.assert;
+
+// function rrr(params) {}
+describe("index.test.ts", () => {
+  it("equal to 1", () => {
+    assert.equal(1, 1);
+  });
+});
